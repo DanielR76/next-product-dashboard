f274dc5bd4ed853c8ea270aaaf4782d42ae879c6
Daniel <danielr2397@gmail.com>
2023-02-27 18:40:06 -0500

    Install libs and organize structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
+import TestComponent from '../components/TestComponent';
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-lg font-bold underline">Hello world!</h1>
-    </main>
-  );
+  return <TestComponent />;
 }

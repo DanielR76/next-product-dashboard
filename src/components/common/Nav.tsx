849797b05dd7b6742875de2e6645c68776920a15
Daniel <danielr2397@gmail.com>
2023-03-04 12:18:43 -0500

    Implement layouts

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,13 +1,13 @@
-import { useRouter } from 'next/router';
+'use client';
+import { usePathname } from 'next/navigation';
 
 export default function Nav() {
-  const router = useRouter();
-  const route = router.pathname.substring(1);
+  const pathName = usePathname();
 
   return (
     <nav className="bg-white shadow">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900 capitalize">{route}</h1>
+        <h1 className="text-3xl font-bold text-gray-900 capitalize">{pathName}</h1>
       </div>
     </nav>
   );
